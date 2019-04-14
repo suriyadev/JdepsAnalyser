4815b9dd2bf4b28e45ab27eceeee95c56f72f8cd
Suriya <35035732+suriyadev@users.noreply.github.com>
2019-04-15 01:08:16 +0530

    Add files via upload

---
+++ JDepsAnalyser.java
@@ -0,0 +1,42 @@
+
+package com.data.services;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class JDepsAnalyser {
+
+	public static void main(String[] args) {
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(
+					"C:\\Users\\surya\\Desktop\\data.json"));
+			String line = reader.readLine();
+			
+			int i = 0;
+			int count = 0;
+			
+			while (line != null) {
+				
+				if(i >= 2 && !line.equals("}")) {
+				
+				String value = line.split("->")[1];	
+					
+				if(value.contains("(not found"))
+					System.out.println(value);
+					count++;
+				}
+				line = reader.readLine();
+				
+			   ++i;
+			} 
+			
+			System.out.println(count+" Missing jars found");
+			
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
